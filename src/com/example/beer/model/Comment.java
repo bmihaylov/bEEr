4bf781fdf8e2867a4ed8a9f88ddf47d49e3c64a9
Bozhidar Mihaylov <bmichailov@gmail.com>
2015-05-28 17:22:11 +0300

    Added Comment Entity

---
+++ src/com/example/beer/model/Comment.java
@@ -0,0 +1,62 @@
+package com.example.beer.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Comment
+ *
+ */
+@Entity
+public class Comment implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String title;
+	
+	@Column(length=500)
+	private String content;
+	
+	@Temporal(TemporalType.DATE)
+	private Date date;
+	
+	private User author;
+	
+	public Comment() {
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+   
+}
