101cfb92d6f79e5dfba58e7a92ab262e3f6eec2c
Bozhidar Mihaylov <bmichailov@gmail.com>
2015-05-28 17:11:11 +0300

    Added Project stub to resolve errors

---
+++ src/com/example/beer/model/Project.java
@@ -0,0 +1,21 @@
+package com.example.beer.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Project
+ *
+ */
+@Entity
+
+public class Project implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Project() {
+		super();
+	}
+   
+}
