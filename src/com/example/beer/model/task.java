cc9cea4a12d525344bf616ee8fe6f79b9e4c5336
Bozhidar Mihaylov <bmichailov@gmail.com>
2015-05-28 17:43:59 +0300

    Added unique constraint on name, project pair in Task Entity

--- src/com/example/beer/model/task.java
+++ src/com/example/beer/model/task.java
@@ -4,6 +4,7 @@ import com.example.beer.model.Project;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -14,92 +15,98 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name="task")
+@Table(name = "task", uniqueConstraints = @UniqueConstraint(columnNames = {
+		"name", "project" }))
 public class Task {
-	
-	public enum TaskStatus {BEGIN, INPROGRESS, FINISHED}
-	
+
+	public enum TaskStatus {
+		BEGIN, INPROGRESS, FINISHED
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
+	@Column(name = "name")
 	String name;
 	String description;
-	
+
 	@Temporal(TemporalType.DATE)
-	Date finalDate;	
-	
+	Date finalDate;
+
 	double estimatedHours;
 	double workedHours;
 
 	@Enumerated(EnumType.STRING)
 	TaskStatus status;
-	
+
 	@ManyToOne
+	@Column(name = "project")
 	Project project;
-	
+
 	void setId(int id) {
 		this.id = id;
 	}
-	
+
 	int getId() {
 		return id;
 	}
-	
+
 	void setName(String name) {
 		this.name = name;
 	}
-	
+
 	String getName() {
 		return name;
 	}
-	
+
 	void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	String getDescription() {
 		return description;
 	}
-	
+
 	void setFinalData(Date finalDate) {
 		this.finalDate = finalDate;
 	}
-	
+
 	Date getFinalDate() {
 		return finalDate;
 	}
-	
+
 	void setHoursWorked(double hours) {
 		this.workedHours = hours;
 	}
-	
+
 	double getHoursWorked() {
 		return workedHours;
 	}
-	
+
 	void setHoursEstimated(double hours) {
 		this.estimatedHours = hours;
 	}
-	
+
 	double getHoursEstimated() {
 		return estimatedHours;
 	}
-	
+
 	void setTaskStatus(TaskStatus status) {
 		this.status = status;
 	}
-	
+
 	TaskStatus getTaskStatus() {
 		return status;
 	}
-	
+
 	void setProject(Project project) {
 		this.project = project;
 	}
-	
+
 	Project getProject() {
 		return project;
 	}
