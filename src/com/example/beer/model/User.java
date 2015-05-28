ebb03ea5ed9e7243c78703543e4c2662f7099e59
Bozhidar Mihaylov <bmichailov@gmail.com>
2015-05-28 17:39:11 +0300

    Added User Entity

--- src/com/example/beer/model/User.java
+++ src/com/example/beer/model/User.java
@@ -1,6 +1,9 @@
 package com.example.beer.model;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.*;
 
 /**
@@ -10,9 +13,83 @@ import javax.persistence.*;
 @Entity
 
 public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1460111118024195065L;
 
 	
-	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(unique=true)
+	private String name;
+	
+	private String salt;
+	private String saltPassHash;
+	
+	private String fullName;
+	
+	private String email;
+	
+	private Set<Task> tasks = new HashSet<Task>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getSaltPassHash() {
+		return saltPassHash;
+	}
+
+	public void setSaltPassHash(String saltPassHash) {
+		this.saltPassHash = saltPassHash;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(Set<Task> tasks) {
+		this.tasks = tasks;
+	}
 
 	public User() {
 		super();
