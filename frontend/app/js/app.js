9aaa77e9485532675a7c333faf6c7dcd4ebabfa5
Hoang Nguyen <hoang.nguyen1992@hotmail.com>
2015-06-28 20:05:52 +0000

    Fix bug of missing ngRoute

--- frontend/app/js/app.js
+++ frontend/app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 // Declare app level module which depends on filters, and services
-var app = angular.module('myApp', [
+var app = angular.module('myApp', ['ngRoute',
 	'oc.lazyLoad',
 	'ui.router',
 	'ui.bootstrap',
