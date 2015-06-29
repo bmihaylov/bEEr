6b2438ca31b7e2d8dc8b879d8edd786ec2dc0a87
Viktor Markov <vikata3@gmail.com>
2015-06-29 23:51:57 +0300

    basic detailed view for ticket

--- frontend/app/js/controllers/ticketCtrl.js
+++ frontend/app/js/controllers/ticketCtrl.js
@@ -1,8 +1,9 @@
 'use strict';
 
 app.controller('ticketCtrl', ['$scope', function($scope){
-    $scope.tickets=[{userName: "Ivan Ivanov", name: "Fix this bug"},
-        {userName: "Petar Ivanov", name: "Fix this bug"},
-        {userName: "Georgi Ivanov", name: "Fix that bug"},
-        {userName: "Bozhidar Ivanov", name: "Fix fuck bug"}];
+    $scope.tickets=[{id: 0, userName: "Ivan Ivanov", name: "Fix this bug", description: "Sample information"},
+        {id: 1, userName: "Petar Ivanov", name: "Fix this bug", description: "Sample information"},
+        {id: 2, userName: "Georgi Ivanov", name: "Fix that bug", description: "Sample information"},
+        {id: 3, userName: "Bozhidar Ivanov", name: "Fix fuck bug", description: "Sample information"}
+    ];
 }]);
