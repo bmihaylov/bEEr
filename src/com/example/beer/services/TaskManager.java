6e386a61ce262702b5a73a0cad28d263e8b9bdd1
Bozhidar Mihaylov <bmichailov@gmail.com>
2015-06-29 17:31:01 +0300

    Rework argument passing for createTask and assignUser to working variants

--- src/com/example/beer/services/TaskManager.java
+++ src/com/example/beer/services/TaskManager.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -15,106 +16,116 @@ import javax.ws.rs.core.Response;
 import com.example.beer.dao.ProjectDAO;
 import com.example.beer.dao.TaskDAO;
 import com.example.beer.dao.UserDAO;
+import com.example.beer.dto.TaskDTO;
 import com.example.beer.model.Project;
 import com.example.beer.model.Task;
-import com.example.beer.model.Task.TaskStatus;
 import com.example.beer.model.User;
 
 @Stateless
 @Path("task")
 public class TaskManager {
-	
+
 	@Inject
 	ProjectDAO projectDAO;
-	
+
 	@Inject
 	TaskDAO taskDAO;
-	
+
 	@Inject
 	UserDAO userDAO;
-	
+
 	@Inject
 	UserContext userContext;
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response createTask(Project project, int assigneeId, String name, String description) {
+	public Response createTask(TaskDTO task) {
 		User currentUser = userContext.getCurrentUser();
 		if (currentUser == null || !currentUser.isAdmin()) {
 			return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).build();
-		} 
-	
-		User assignee = userDAO.getUserById(assigneeId);
-		
+		}
+
+		User assignee = userDAO.getUserById(task.getAssigneeId());
+
 		if (assignee == null) {
 			return Response.status(HttpURLConnection.HTTP_NOT_FOUND).build();
 		}
 
-		if (!taskDAO.addTask(name, description, assignee)) {
-			return Response.status(HttpURLConnection.HTTP_NOT_ACCEPTABLE).build();
+		Project project = projectDAO.findProjectById(task.getProjectId());
+		if (project == null) {
+			return Response.status(HttpURLConnection.HTTP_NOT_FOUND).build();
 		}
-		
-		Task task = taskDAO.findTaskByName(name);
-		
-		if (taskDAO.addToProject(project, task)) {
+
+		String name = task.getName();
+		if (!taskDAO.addTask(name, task.getDescription(), task.getFinalDate(),
+				task.getEstimatedHours(), project, assignee)) {
+			return Response.status(HttpURLConnection.HTTP_NOT_ACCEPTABLE)
+					.build();
+		}
+
+		Task foundTask = taskDAO.findTaskByName(name);
+
+		if (taskDAO.addToProject(project, foundTask)) {
 			return Response.status(HttpURLConnection.HTTP_OK).build();
-        } else {
-        	return Response.status(HttpURLConnection.HTTP_NOT_ACCEPTABLE).build();
-        }
+		} else {
+			return Response.status(HttpURLConnection.HTTP_NOT_ACCEPTABLE)
+					.build();
+		}
 	}
-	
+
 	@POST
+	@Path("assign/{taskId}/{userId}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response assignUser(User user, Task task) {
+	public Response assignUser(@PathParam("taskId") int taskId,
+			@PathParam("userId") int assigneeId) {
 		User currentUser = userContext.getCurrentUser();
 		if (currentUser == null) {
 			return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).build();
-		} 
-		
-		if (taskDAO.assignUser(task, user)) {
-			return Response.status(HttpURLConnection.HTTP_OK).build();
-		}
-		
-		return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).build();
-	}
-	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response changeStatus(Task task, TaskStatus status) {
-		User currentUser = userContext.getCurrentUser();
-		
-		if (currentUser == null) {
-			return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).build(); 
-		}
-		
-		if (!task.getAssignee().equals(currentUser) && !currentUser.isAdmin()) {
-			return Response.status(HttpURLConnection.HTTP_BAD_REQUEST).build(); 
 		}
 
-		if (taskDAO.changeStatus(task, status)) {
+		if (taskDAO.assignUser(taskId, assigneeId)) {
 			return Response.status(HttpURLConnection.HTTP_OK).build();
 		}
-		
-		return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).build();
+
+		return Response.status(HttpURLConnection.HTTP_NOT_FOUND).build();
 	}
-	
+
+	// @POST
+	// @Consumes(MediaType.APPLICATION_JSON)
+	// public Response changeStatus(Task task, TaskStatus status) {
+	// User currentUser = userContext.getCurrentUser();
+	//
+	// if (currentUser == null) {
+	// return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).build();
+	// }
+	//
+	// if (!task.getAssignee().equals(currentUser) && !currentUser.isAdmin()) {
+	// return Response.status(HttpURLConnection.HTTP_BAD_REQUEST).build();
+	// }
+	//
+	// if (taskDAO.changeStatus(task, status)) {
+	// return Response.status(HttpURLConnection.HTTP_OK).build();
+	// }
+	//
+	// return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).build();
+	// }
+	//
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response findTaskById(int taskId) {
 		User currentUser = userContext.getCurrentUser();
-		
+
 		if (currentUser == null) {
-			return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).build(); 
+			return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).build();
 		}
-		
+
 		Task foundTask = taskDAO.getTaskById(taskId);
-		
+
 		if (foundTask == null) {
-			return Response.status(HttpURLConnection.HTTP_NOT_FOUND).build(); 
+			return Response.status(HttpURLConnection.HTTP_NOT_FOUND).build();
 		}
 
 		return Response.ok(foundTask).build();
 	}
-	
-	
+
 }
