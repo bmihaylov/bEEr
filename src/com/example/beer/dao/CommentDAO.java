f76092dc21aacbd8947aa1599299f0214f1f01e2
IceCubeDev <ivandortulov@yahoo.com>
2015-06-29 13:18:10 +0300

    Implemented CommentDAO

--- src/com/example/beer/dao/CommentDAO.java
+++ src/com/example/beer/dao/CommentDAO.java
@@ -1,8 +1,49 @@
 package com.example.beer.dao;
 
 import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import com.example.beer.model.Comment;
+import com.example.beer.model.Task;
+import com.example.beer.model.User;
 
 @Singleton
 public class CommentDAO {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public boolean addComment(Task task, Comment comment) {
+		Comment foundComment = findComment(task, 
+				comment.getAuthor(), comment.getTitle());
+		
+		if (foundComment == null) {
+			task.addComment(comment);
+			em.persist(task);
+			em.persist(comment);
+			return true;
+		} else {
+			return false;
+		}
+	}
 
+	private Comment findComment(Task task, User author, String title) {
+		String idQueryText = "SELECT c FROM Comment c WHERE c.title  = :title AND "
+														+ "	c.author = :author AND "
+														+ " c.task = :task";
+		TypedQuery<Comment> idQuery = em.createQuery(idQueryText, Comment.class);
+		idQuery.setParameter("title", title);
+		idQuery.setParameter("author", author.getName());
+		idQuery.setParameter("task", task);
+		
+		try {
+			return idQuery.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+	
 }
